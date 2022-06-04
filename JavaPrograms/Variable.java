0e26ee77966995876db3a77e7ca5d8fb7ac5ed79
rnagathann <rnagathann@gmail.com>
2022-06-04 10:00:13 -0500

    add new files

--- JavaPrograms/Variable.java
+++ JavaPrograms/Variable.java
@@ -1,7 +1,8 @@
-class DemoJava
+class Variable
  {
    public static void main(String[] args)
      {
-      System.out.println("Hello World");
+      System.out.println("Variable");
      }
   }   
+
